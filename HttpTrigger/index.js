608fb819b8106defde2db74c0dd1696380b9a998
Nakkeeran Natarajan <navcse06@gmail.com>
2018-11-03 11:30:50 -0700

    changed logic

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,7 +1,6 @@
-//var AuthenticationContext = require('adal-node').AuthenticationContext;
 var request = require('request-promise');
 var spauth = require('node-sp-auth');
-var siteUrl = 'https://cosmo2013.sharepoint.com/sites/DigitalPlace';
+var siteUrl = 'https://nakkeerann.sharepoint.com';
 var clientId = 'ac75fede-95f5-4c00-9256-f163d1beb46a';
 var clientSecret = '8T3dEmEcqu2U5pYpwwgfhwECk7vc0ybszB7bUaPQ9qI=';
 module.exports = async function (context, req) {
@@ -14,7 +13,7 @@ module.exports = async function (context, req) {
             status: 200,
             body: data
         };
-        var restApi = "https://cosmo2013.sharepoint.com/sites/DigitalPlace/_api/web/lists/getbytitle('Topics')/items?$select=Title,FileRef,IsExternal,TopicImage,Body&$filter=(IsExternal eq 0)";
+        var restApi = "https://nakkeerann.sharepoint.com/_api/web/lists/getbytitle('Books')/items?$select=Title";
         request.get({
             url: restApi,
             headers: data.headers,
@@ -26,17 +25,5 @@ module.exports = async function (context, req) {
                 body: listResults
             };    
         });
-    /*if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, 
-            body: "Hello, " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }*/
     });
 };
